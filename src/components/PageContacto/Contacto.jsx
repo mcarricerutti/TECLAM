842aab89a1eb6f3bc160a5ca66ca775ceac8c8d9
mcarricerutti <martinacarri345@gmail.com>
2022-10-31 10:51:46 -0300

    Images

--- src/components/PageContacto/Contacto.jsx
+++ src/components/PageContacto/Contacto.jsx
@@ -5,7 +5,7 @@ const Contacto = () => {
             <div className="row">
             <p className="titleContacto">MANTENETE EN CONTACTO</p>
                 <div className="col-lg-4">
-                    <img className="imgContacto" src="https://res.cloudinary.com/dpruqtqw2/image/upload/v1666968353/keyboards_vntc49.png" alt="imgContacto"/>
+                    
                 </div>
                 <div className="col-lg-4">
                     <p className="textContacto">- contacto@teclam.com</p>
@@ -19,7 +19,7 @@ const Contacto = () => {
                     <h4 className="infoContac">16pm-20pm</h4>
                 </div>
                 <div className="col-lg-4">
-                   <img className="imgContacto" src="https://res.cloudinary.com/dpruqtqw2/image/upload/v1666968353/keyboards_vntc49.png" alt="imgContacto"/>
+                   
                 </div>
 
             </div>
--- src/components/seleccionProd/Select.jsx
+++ src/components/seleccionProd/Select.jsx
@@ -7,20 +7,20 @@ const Select = () => {
             <div className="row">
                 <div className="col-lg-4">
                     <Link to="/products/teclados">
-                        <img src="https://res.cloudinary.com/dpruqtqw2/image/upload/v1666368752/Captura_de_pantalla_264_s2sfrf.png" alt="imgCategory" className="imgSelect"/>
+                        <img src="https://res.cloudinary.com/dpruqtqw2/image/upload/v1667224147/Captura_de_pantalla_264_ye6xt6.png" alt="imgCategory" className="imgSelect"/>
                     </Link>
                 </div>
                 
                 <div className="col-lg-4">
                     <Link to="/products/esqueletoTeclado">
-                        <img src="https://res.cloudinary.com/dpruqtqw2/image/upload/v1666368763/Captura_de_pantalla_265_itvaqx.png" alt="imgCategory" className="imgSelect"/>
+                        <img src="https://res.cloudinary.com/dpruqtqw2/image/upload/v1667224133/Captura_de_pantalla_265_irbwor.png" alt="imgCategory" className="imgSelect"/>
                     </Link>
                 </div>
             
 
                 <div className="col-lg-4">
                     <Link to="/products/teclas">
-                        <img src="https://res.cloudinary.com/dpruqtqw2/image/upload/v1666368778/Captura_de_pantalla_266_e3dky6.png" alt="imgCategory" className="imgSelect"/>
+                        <img src="https://res.cloudinary.com/dpruqtqw2/image/upload/v1667224138/Captura_de_pantalla_266_qpu2xq.png" alt="imgCategory" className="imgSelect"/>
                     </Link>
                 </div>
                
